84d6318a844d87701760bb423bcf72da4aa5364d
lih <541573560@qq.com>
2018-04-11 14:28:27 +0800

    add student massage check detil page

--- academy_control/urls.py
+++ academy_control/urls.py
@@ -5,5 +5,6 @@ from academy_control import views
 app_name = 'academy_control'
 
 urlpatterns = [
-    url(r'^massage',views.check_stu_massage,name='control_stu_mas'),
+    url(r'^massage/',views.check_stu_massage,name='control_stu_mas'),
+    url(r'^detil/(\w+)/',views.detil_massage,name='show_detil'),
 ]
--- academy_control/views.py
+++ academy_control/views.py
@@ -8,31 +8,48 @@ def check_stu_massage(request):
         student = student_personal_massage.objects.filter(academy__contains='计算机')
         return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'guojiao':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='国际教育')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'jingjiguanli':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='经济管理')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'jidian':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='机电')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'cailiao':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='材料')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'huaxuegongcheng':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='化学工程')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'huaxueshengming':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='化学与生命')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'yishusheji':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='艺术')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'renwen':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='人文')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'makesi':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='马克思')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'jichu':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='基础')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'waiguoyu':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='外国语')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     elif user_name == 'yingyong':
-        pass
+        student = student_personal_massage.objects.filter(academy__contains='应用')
+        return render(request, 'control/control.html', context={'stu_grouop': student})
     else:
         return HttpResponse('您尚未登录')
 
-def detil_massage():
-    pass
+def detil_massage(request,stu_id):
+    from student.models import student_personal_massage,parents,update_massage,gratudated_and_employ
+    student = student_personal_massage.objects.get(stu_id = stu_id)
+    parent = parents.objects.filter(stu_id = stu_id)
+    update = update_massage.objects.filter(stu_id = stu_id)
+    gratuate = gratudated_and_employ.objects.filter(stu_id = stu_id)
+    return render(request,'control/detil_mas.html',context={'student':student,'parent':parent,'update':update,'gratuate':gratuate})
